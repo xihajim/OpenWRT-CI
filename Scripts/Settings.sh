bffb5e38fd38ccb25698e79d2d43ee7b5d2dcfaf
xihajim <73975896+xihajim@users.noreply.github.com>
2024-09-02 10:23:32 +0800

    Update Settings.sh
    更新luci-app-passwall

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -53,5 +53,6 @@ if [[ $WRT_REPO == *"lede"* ]]; then
 else
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+#	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+ 	echo "CONFIG_PACKAGE_luci-app-passwall=y" >> ./.config
 fi
